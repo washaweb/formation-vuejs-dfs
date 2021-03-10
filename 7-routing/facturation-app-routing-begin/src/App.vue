a3dcaa3125a76cf8884aecd67836a937739be22e
Jérôme Poslednik <webmaster@washaweb.com>
2021-03-10 10:45:15 +0100

    preparation de vue-router

---
+++ 7-routing/facturation-app-routing-begin/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Navigation />
+    <EditBill />
+  </div>
+
+</template>
+
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+import EditBill from '@/views/EditBill.vue'
+// controleur du composant
+export default {
+  name: 'App',
+  components: {
+    EditBill,
+    Navigation
+  }
+}
+</script>
+
+<style>
+
+/* REMPLACÉ PAR BOOTSTRAP-VUE : import du framework (css) Boostrap */
+/* @import url('https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css'); */
+
+/* import du framework (css) Font-Awesome */
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css');
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ 7-routing/facturation-app-routing-begin/src/components/nav-menu/NavMenuItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li class="lead">
+    <span class="fa-li">
+      <i class="fas fa-angle-right"></i>
+    </span>
+    <a
+      :class="{
+        'text-primary': link.active,
+        'text-secondary': !link.active
+      }"
+      :href="link.path">
+      {{ link.name }}
+    </a>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'NavMenuItem',
+    props: {
+      link: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
