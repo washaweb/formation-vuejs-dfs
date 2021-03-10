0f65d4ee51e1d959ba94bbcf1b04616b6624fe0b
Jérôme Poslednik <webmaster@washaweb.com>
2021-03-10 10:14:30 +0100

    correction integration des composants navbar et navigation

---
+++ 6-components/facturation-app-cmpt-end/src/components/nav-menu/NavMenu.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul class="fa-ul my-4">
+    <slot></slot>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name:'NavMenu'
+  }
+</script>
---
+++ 6-components/facturation-app-cmpt-end/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// bootstrap-vue imports
+import './plugins/bootstrap-vue'
+import './globalComponent'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
