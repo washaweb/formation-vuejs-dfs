ee61181ab8b50775fcd84200e4e3916cdac9a0cc
Jérôme Poslednik <webmaster@washaweb.com>
2021-03-11 15:04:38 +0100

    ajout des routes delete, post, put

--- 9-backend-api/facturation-app-backend-start/backend/controllers/bills.js
+++ 9-backend-api/facturation-app-backend-start/backend/controllers/bills.js
@@ -1,4 +1,4 @@
-//contrôleur de la route /bills
+//contrôleur de la route GET /bills
 exports.getList = (req, res) => {
   res.status(200).json([
     { bill: 'test' },
@@ -6,11 +6,32 @@ exports.getList = (req, res) => {
   ])
 }
 
-//contrôleur de la route /bills/:id
+//contrôleur de la route GET /bills/:id
 exports.getItem = (req, res) => {
   const myId = req.params.id
   const myBody = req.body
   const toto = req.query.toto
 
   res.status(200).json({ id: myId, toto, myBody  })
+}
+
+//contrôleur de la route POST /bills/
+exports.postItem = (req, res) => {
+  console.log(req.body)
+  //sauvegarde en BDD, retourne la donnée sauvegardée
+  res.status(200).json({ myBody: req.body })
+}
+
+//contrôleur de la route PUT /bills/
+exports.putItem = (req, res) => {
+  console.log(req.body)
+  //sauvegarde en BDD, retourne la donnée sauvegardée
+  res.status(200).json({ myBody: req.body  })
+}
+
+//contrôleur de la route DELETE /bills/
+exports.deleteItem = (req, res) => {
+  console.log(req.params.id)
+  //sauvegarde en BDD, retourne la donnée sauvegardée
+  res.status(200).json({ id: req.params.id  })
 }
